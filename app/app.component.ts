c91496370c63b90497890e91bbe0b79081b19a39
Steven Van Roye <svr1m2f@admin.colruyt.int>
2019-01-02 13:41:24 +0100

    safety operator

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
-
+interface Child {
+  name: string;
+  age: number;
+}
 
 interface Passenger {
   id: number;
@@ -8,6 +11,7 @@ interface Passenger {
   lname: string;
   checkedin: boolean;
   checkedInDate: number | null;
+  children: Child[] | null;
 }
 
 @Component({
@@ -24,6 +28,7 @@ interface Passenger {
           <p>{{ passenger | json }} </p>
           <div class="date">Checked in date: 
           {{passenger.checkedInDate ? (passenger.checkedInDate | date: 'hh:mm:ss d/M/y') : 'Not checked in'}}</div>
+          <p> Children : {{ passenger.children?.length || 0}}
         </li>
       </ul>
     </div>
@@ -32,10 +37,31 @@ interface Passenger {
 export class AppComponent {
 
   passengers: Passenger[] = [
-    {"id": 1, "fname": "Bart", "lname": "Peeters", "checkedin": true, "checkedInDate": 1546255457000},
-    {"id": 2, "fname": "Koen", "lname": "Wouters", "checkedin": true, "checkedInDate": 1546241286000},
-    {"id": 3, "fname": "Kris", "lname": "Wouters", "checkedin": false, "checkedInDate": null},
-    {"id": 4, "fname": "Niels", "lname": "Destadsbader", "checkedin": true, "checkedInDate": 1546155985000}
+    { "id": 1, 
+      "fname": "Bart", 
+      "lname": "Peeters", 
+      "checkedin": true, 
+      "checkedInDate": 1546255457000, 
+      "children": null},
+    { "id": 2, 
+      "fname": "Koen", 
+      "lname": "Wouters", 
+      "checkedin": true, 
+      "checkedInDate": 1546241286000, 
+      "children": [{"name": "Kim", "age": 12}, {"name": "Tim", "age": 16}]
+    },
+    { "id": 3, 
+      "fname": "Kris", 
+      "lname": "Wouters", 
+      "checkedin": false, 
+      "checkedInDate": null, 
+      "children": null},
+    { "id": 4, 
+      "fname": "Niels", 
+      "lname": "Destadsbader", 
+      "checkedin": true, 
+      "checkedInDate": 1546155985000, 
+      "children": null}
   ];
 
 }
