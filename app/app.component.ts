724ce0ac8a78ba58a8ebbd0967d5b92fc4f6e011
Steven Van Roye <svr1m2f@admin.colruyt.int>
2019-01-02 12:04:49 +0100

    ngFor and NgIf

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,40 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number;
+  fname: string;
+  lname: string;
+  checkedin: boolean;
+}
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   templateUrl: `
     <div class="app">
-      <input type='text' #username>
-      <button (click)="handleClick(username.value)">Get value</button>
-      <div>
-        {{ name }}
-      </div>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index">
+          {{i}}: {{passenger.fname}}
+        </li>
+      </ul>
+
+      <ul>
+        <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
+          <li>
+            {{i}}: {{passenger.fname}}
+          </li>
+        </template>
+    </ul>
     </div>
   `
 })
 export class AppComponent {
 
-  name: string = "Steven";
+  passengers: Passenger[] = [
+    {"id": 1, "fname": "Bart", "lname": "Peeters", "checkedin": true},
+    {"id": 2, "fname": "Koen", "lname": "Wouters", "checkedin": true},
+    {"id": 3, "fname": "Kris", "lname": "Wouters", "checkedin": false},
+    {"id": 4, "fname": "Niels", "lname": "Destadsbader", "checkedin": true}
+  ];
 
-  handleClick(value: string) {
-    this.name = value;
-  }
 }
